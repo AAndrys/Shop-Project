288f71c151c63c7a71671ac7c590f77c9b4ed3f9
Andrys <aadam606@gmail.com>
2020-08-19 21:20:18 +0200

    deploy gh

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
         <NavBar />
         <Switch>
           <Route 
-            path='/Shop-Project'
+            path='/'
             exact 
             render={(props) => <Home {...props} /> } 
           />
